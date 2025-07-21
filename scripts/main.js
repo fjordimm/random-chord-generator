722b0e4c708c881c893748abf1ed5eb5b4ebe065
fjordimm <fjordimm@gmail.com>
2025-07-20 21:52:03 -0700

    starting to make key clicking work

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 
-const keydownArray = [];
-for (let i = 0; i <= 127; i++) { keydownArray[i] = false; }
-let isSustainPedalDown = false;
+const midiKeydownArray = [];
+for (let i = 0; i <= 127; i++) { midiKeydownArray[i] = false; }
+let midiPedalIsDown = false;
 
 let currentChordNotes = [];
 
@@ -18,192 +18,242 @@ const sliderHalfDiminished7 = document.querySelector("#slider-half-diminished7")
 const sliderDiminished7 = document.querySelector("#slider-diminished7");
 const chordTextarea = document.getElementById("chord-textarea");
 
-function onPianoKeyClicked(keyNum) {
-    alert("hi");
+function main()
+{
+    if (!window.isSecureContext)
+    {
+        alert("Window is not in a 'secure context'.");
+    }
+    else
+    {
+        // navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+
+        for (let i = 0; i < 12; i++)
+        {
+            keyElem = document.getElementById(`key_${i}`);
+            
+            keyElem.addEventListener("mousedown", () =>
+            { whenKeyDown(i); });
+
+            document.addEventListener("mouseup", () =>
+            { whenKeyUp(i); });
+        }
+
+        generateRandomChord();
+    }
 }
 
-function main() {
-	if (!window.isSecureContext)
-	{
-		alert("Window is not in a 'secure context'.");
-	}
-	else
-	{
-		// navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
-
-		generateRandomChord();
-	}
+function onMIDISuccess(midiAccess)
+{
+    whenMidiAvailable(midiAccess);
 }
 
-function onMIDISuccess(midiAccess) {
-	whenMidiAvailable(midiAccess);
+function onMIDIFailure(msg)
+{
+    alert("Midi access was denied (see console for more details).");
+    console.error(`Failed to get MIDI access - ${msg}`);
 }
 
-function onMIDIFailure(msg) {
-	alert("Midi access was denied (see console for more details).");
-	console.error(`Failed to get MIDI access - ${msg}`);
+function whenMidiAvailable(midiAccess)
+{
+    if (midiAccess)
+    {
+        midiAccess.inputs.forEach((entry) =>
+        {
+            entry.onmidimessage = onMidiMsg;
+        });
+    } else
+    {
+        alert("MIDIAccess object was not initialized.");
+    }
 }
 
-function whenMidiAvailable(midiAccess) {
-	if (midiAccess) {
-		midiAccess.inputs.forEach((entry) => {
-			entry.onmidimessage = onMidiMsg;
-		});
-	} else {
-		alert("MIDIAccess object was not initialized.");
-	}
+function onMidiMsg(event)
+{
+    let message = `MIDI message received at timestamp ${event.timeStamp}[${event.data.length} bytes]: `;
+    for (const character of event.data)
+    {
+        message += `${character.toString()} `;
+    }
+    // console.log(message);
+
+    let action = null;
+    if (event.data[0] == 144) { action = "down"; }
+    if (event.data[0] == 128) { action = "up"; }
+    if (event.data[0] == 176) { action = "pedal"; }
+    let keyCode = event.data[1];
+    let velocity = event.data[2];
+
+    onMidiKeyPress(action, keyCode, velocity);
 }
 
-function onMidiMsg(event) {
-	let message = `MIDI message received at timestamp ${event.timeStamp}[${event.data.length} bytes]: `;
-	for (const character of event.data) {
-		message += `${character.toString()} `;
-	}
-	// console.log(message);
+function onMidiKeyPress(action, keyCode, velocity)
+{
+    if (action === "down")
+    {
+        midiKeydownArray[keyCode] = true;
 
-	let action = null;
-	if (event.data[0] == 144) { action = "down"; }
-	if (event.data[0] == 128) { action = "up"; }
-	if (event.data[0] == 176) { action = "pedal"; }
-	let keyCode = event.data[1];
-	let velocity = event.data[2];
+        let keyElem = codeToKeyElem(keyCode);
+        if (!keyElem)
+        {
+            console.warn(`The key code ${keyCode} does not correspond to an existing html element.`);
+        } else
+        {
+            const isCorrect = currentChordNotes.includes((keyCode - startingC) % 12);
+            keyElem.style.backgroundColor = isCorrect ? keyColorCorrect : keyColorIncorrect;
+        }
+    } else if (action === "up")
+    {
+        midiKeydownArray[keyCode] = false;
 
-	onMidiKeyPress(action, keyCode, velocity);
+        let keyElem = codeToKeyElem(keyCode);
+        if (!keyElem)
+        {
+            console.warn(`The key code ${keyCode} does not correspond to an existing html element.`);
+        } else
+        {
+            keyElem.style.backgroundColor = isBlackNote(keyCodeToNote(keyCode)) ? keyColorBlack : keyColorWhite;
+        }
+    } else if (action === "pedal")
+    {
+        if (keyCode === 64)
+        {
+            midiPedalIsDown = velocity !== 0;
+
+            if (midiPedalIsDown)
+            {
+                if (pedalCheckbox.checked)
+                {
+                    generateRandomChord();
+                }
+            }
+        }
+    }
 }
 
-function onMidiKeyPress(action, keyCode, velocity) {
-	if (action === "down") {
-		keydownArray[keyCode] = true;
-
-		let keyElem = codeToKeyElem(keyCode);
-		if (!keyElem) {
-			console.warn(`The key code ${keyCode} does not correspond to an existing html element.`);
-		} else {
-			const isCorrect = currentChordNotes.includes((keyCode - startingC) % 12);
-			keyElem.style.backgroundColor = isCorrect ? keyColorCorrect : keyColorIncorrect;
-		}
-	} else if (action === "up") {
-		keydownArray[keyCode] = false;
-
-		let keyElem = codeToKeyElem(keyCode);
-		if (!keyElem) {
-			console.warn(`The key code ${keyCode} does not correspond to an existing html element.`);
-		} else {
-			keyElem.style.backgroundColor = isBlackNote(keyCodeToNote(keyCode)) ? keyColorBlack : keyColorWhite;
-		}
-	} else if (action === "pedal") {
-		if (keyCode === 64) {
-			isSustainPedalDown = velocity !== 0;
-			
-			if (isSustainPedalDown) {
-				if (pedalCheckbox.checked) {
-					generateRandomChord();
-				}
-			}
-		}
-	}
+function whenKeyDown(key)
+{
+    document.getElementById(`key_${key}`).classList.add("pressed");
 }
 
-function generateRandomChord() {
-	const root = Math.floor(Math.random() * 12);
-	const noteName = noteValToName(root, (Math.floor(Math.random() * 2) === 0) ? false : true);
-
-	const suffixAndIntervals = getSuffixAndIntervals();
-	const suffix = suffixAndIntervals[0];
-	const intervals = suffixAndIntervals[1];
-
-	chordTextarea.innerHTML = noteName + " " + suffix;
-	
-	currentChordNotes = [];
-	for (const interval of intervals) {
-		currentChordNotes.push((root + interval) % 12);
-	}
+function whenKeyUp(key)
+{
+    document.getElementById(`key_${key}`).classList.remove("pressed");
 }
 
-function noteValToName(val, sharpInsteadOfFlat = false) {
-	switch (val) {
-		case 0:
-			return "C";
-		case 1:
-			return sharpInsteadOfFlat ? "C#" : "Db";
-		case 2:
-			return "D";
-		case 3:
-			return sharpInsteadOfFlat ? "D#" : "Eb";
-		case 4:
-			return "E";
-		case 5:
-			return "F";
-		case 6:
-			return sharpInsteadOfFlat ? "F#" : "Gb";
-		case 7:
-			return "G";
-		case 8:
-			return sharpInsteadOfFlat ? "G#" : "Ab";
-		case 9:
-			return "A";
-		case 10:
-			return sharpInsteadOfFlat ? "A#" : "Bb";
-		case 11:
-			return "B";
-		default:
-			return "(ErrorNote)";
-	}
+function generateRandomChord()
+{
+    const root = Math.floor(Math.random() * 12);
+    const noteName = noteValToName(root, (Math.floor(Math.random() * 2) === 0) ? false : true);
+
+    const suffixAndIntervals = getSuffixAndIntervals();
+    const suffix = suffixAndIntervals[0];
+    const intervals = suffixAndIntervals[1];
+
+    chordTextarea.innerHTML = noteName + " " + suffix;
+
+    currentChordNotes = [];
+    for (const interval of intervals)
+    {
+        currentChordNotes.push((root + interval) % 12);
+    }
 }
 
-function getSuffixAndIntervals() {
-	const weightMajor = parseInt(sliderMajor.value);
-	const weightMinor = parseInt(sliderMinor.value);
-	const weightDiminished = parseInt(sliderDiminished.value);
-	const weightMajor7 = parseInt(sliderMajor7.value);
-	const weightDominant7 = parseInt(sliderDominant7.value);
-	const weightMinor7 = parseInt(sliderMinor7.value);
-	const weightHalfDiminished7 = parseInt(sliderHalfDiminished7.value);
-	const weightDiminished7 = parseInt(sliderDiminished7.value);
-	let randVal = Math.random() * (weightMajor + weightMinor + weightDiminished + weightMajor7 + weightDominant7 + weightMinor7 + weightHalfDiminished7 + weightDiminished7);
-	{
-		randVal -= weightMajor;
-		if (randVal < 0) {
-			return ["", [0, 4, 7]];
-		}
+function noteValToName(val, sharpInsteadOfFlat = false)
+{
+    switch (val)
+    {
+        case 0:
+            return "C";
+        case 1:
+            return sharpInsteadOfFlat ? "C#" : "Db";
+        case 2:
+            return "D";
+        case 3:
+            return sharpInsteadOfFlat ? "D#" : "Eb";
+        case 4:
+            return "E";
+        case 5:
+            return "F";
+        case 6:
+            return sharpInsteadOfFlat ? "F#" : "Gb";
+        case 7:
+            return "G";
+        case 8:
+            return sharpInsteadOfFlat ? "G#" : "Ab";
+        case 9:
+            return "A";
+        case 10:
+            return sharpInsteadOfFlat ? "A#" : "Bb";
+        case 11:
+            return "B";
+        default:
+            return "(ErrorNote)";
+    }
+}
 
-		randVal -= weightMinor;
-		if (randVal < 0) {
-			return ["m", [0, 3, 7]];
-		}
+function getSuffixAndIntervals()
+{
+    const weightMajor = parseInt(sliderMajor.value);
+    const weightMinor = parseInt(sliderMinor.value);
+    const weightDiminished = parseInt(sliderDiminished.value);
+    const weightMajor7 = parseInt(sliderMajor7.value);
+    const weightDominant7 = parseInt(sliderDominant7.value);
+    const weightMinor7 = parseInt(sliderMinor7.value);
+    const weightHalfDiminished7 = parseInt(sliderHalfDiminished7.value);
+    const weightDiminished7 = parseInt(sliderDiminished7.value);
+    let randVal = Math.random() * (weightMajor + weightMinor + weightDiminished + weightMajor7 + weightDominant7 + weightMinor7 + weightHalfDiminished7 + weightDiminished7);
+    {
+        randVal -= weightMajor;
+        if (randVal < 0)
+        {
+            return ["", [0, 4, 7]];
+        }
 
-		randVal -= weightDiminished;
-		if (randVal < 0) {
-			return ["dim", [0, 3, 6]];
-		}
+        randVal -= weightMinor;
+        if (randVal < 0)
+        {
+            return ["m", [0, 3, 7]];
+        }
 
-		randVal -= weightMajor7;
-		if (randVal < 0) {
-			return ["M7", [0, 4, 7, 11]];
-		}
+        randVal -= weightDiminished;
+        if (randVal < 0)
+        {
+            return ["dim", [0, 3, 6]];
+        }
 
-		randVal -= weightDominant7;
-		if (randVal < 0) {
-			return ["7", [0, 4, 7, 10]];
-		}
+        randVal -= weightMajor7;
+        if (randVal < 0)
+        {
+            return ["M7", [0, 4, 7, 11]];
+        }
 
-		randVal -= weightMinor7;
-		if (randVal < 0) {
-			return ["m7", [0, 3, 7, 10]];
-		}
+        randVal -= weightDominant7;
+        if (randVal < 0)
+        {
+            return ["7", [0, 4, 7, 10]];
+        }
 
-		randVal -= weightHalfDiminished7;
-		if (randVal < 0) {
-			return ["hdim7", [0, 3, 6, 10]];
-		}
+        randVal -= weightMinor7;
+        if (randVal < 0)
+        {
+            return ["m7", [0, 3, 7, 10]];
+        }
 
-		randVal -= weightDiminished7;
-		if (randVal < 0) {
-			return ["dim7", [0, 3, 6, 9]];
-		}
-	}
+        randVal -= weightHalfDiminished7;
+        if (randVal < 0)
+        {
+            return ["hdim7", [0, 3, 6, 10]];
+        }
 
-	return "(ErrorSuffix)";
+        randVal -= weightDiminished7;
+        if (randVal < 0)
+        {
+            return ["dim7", [0, 3, 6, 9]];
+        }
+    }
+
+    return "(ErrorSuffix)";
 }
 
 main();
